2b690c01c7401fa234005b511e2e5352263dad98
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-02-17 08:02:15 +0300

    WIP: Cache global folders

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'pathname'
+require 'English'
 
 def get_env_variable(key)
   return nil if ENV[key].nil? || ENV[key].strip.empty?
@@ -11,6 +12,52 @@ ac_cache_included_paths = get_env_variable('AC_CACHE_INCLUDED_PATHS') || abort('
 ac_cache_excluded_paths = get_env_variable('AC_CACHE_EXCLUDED_PATHS')
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR') || abort('Repository path must be defined.')
 
+@cache = 'ac_cache'
+
 puts ac_cache_included_paths
 puts ac_cache_excluded_paths
 puts ac_repository_path
+puts ''
+
+system("mkdir -p #{@cache}")
+
+def run_command(command)
+  puts "@@[command] #{command}"
+  exit $CHILD_STATUS.exitstatus unless system(command)
+end
+
+def cache_global_path(base_path, included_path, base_replace_with = '')
+  puts "Global Include: #{included_path}"
+
+  included_path = included_path[(base_path.length + 1)..-1]
+  base_path = base_replace_with unless base_replace_with.empty?
+  glob_pattern = "#{base_path}/#{included_path}"
+
+  zip_file = "#{@cache}/#{glob_pattern.gsub('/', '_')}.zip"
+  zip = "zip -r -FS #{zip_file}"
+  Dir.glob(glob_pattern).each do |f|
+    zip += " #{f}"
+  end
+  run_command("#{zip} > #{zip_file}.log")
+  system("ls -lh #{zip_file}")
+end
+
+def cache_repository_path(base_path, included_path)
+  puts "Repository Include: #{included_path}"
+
+  Dir.glob("#{base_path}/#{included_path}").each do |f|
+    puts f
+  end
+end
+
+ac_cache_included_paths.split(':').each do |included_path|
+  included_path = included_path[1..-1] if !included_path.empty? && included_path[0] == '/'
+  next if included_path.empty?
+
+  # @todo: Check $home path for other types of agents and build profiles
+  if included_path.start_with?('$HOME')
+    cache_global_path('$HOME', included_path, '/setup')
+  else
+    cache_repository_path(ac_repository_path, included_path)
+  end
+end
--- tests/cache_push.sh
+++ tests/cache_push.sh
@@ -4,14 +4,36 @@ set -o nounset
 set -o errexit
 
 testApp="https://github.com/csonuryilmaz/TextPad.git"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false"
 
-export AC_CACHE_INCLUDED_PATHS="local.properties:.gradle:$HOME/.gradle"
-export AC_CACHE_EXCLUDED_PATHS=""
+# Workflow step input variables (component.yml>inputs)
+export AC_CACHE_INCLUDED_PATHS="local.properties:.gradle:\$HOME/.gradle"
+#AC_CACHE_INCLUDED_PATHS="local.properties:.gradle:\$HOME/.gradle:/foo::/:bar"
+export AC_CACHE_EXCLUDED_PATHS="\$HOME/.gradle/caches/*.lock"
 export AC_REPOSITORY_DIR="$HOME/app/workflow_data/tjrdzp35.isa/_appcircle_temp/Repository"
 
 mkdir -p $AC_REPOSITORY_DIR
-[ "$(ls -A ${AC_REPOSITORY_DIR})" ] || git clone $testApp $AC_REPOSITORY_DIR
+if [ ! "$(ls -A $AC_REPOSITORY_DIR)" ]; then 
+  rm -rf $HOME/.gradle
+  git clone $testApp $AC_REPOSITORY_DIR
+fi
 
-echo "@@[section:begin] Step started: Cache Pull"
+if [ ! -f "$AC_REPOSITORY_DIR/local.properties" ]; then
+  echo "sdk.dir=/home/onur/Android/Sdk" > $AC_REPOSITORY_DIR/local.properties
+fi
+
+if [ ! -d "$AC_REPOSITORY_DIR/app/build" ]; then
+  cwd=$(pwd)
+  cd $AC_REPOSITORY_DIR
+  chmod +x ./gradlew && ./gradlew --build-cache app:assembleDebug
+  cd $cwd
+fi
+
+if [ ! -L "/setup" ]; then
+  sudo ln -sf $HOME /setup
+fi
+
+echo ""
+echo "@@[section:begin] Step started: Cache Push"
 ruby main.rb
-echo "@@[section:end] Step completed: Cache Pull"
+echo "@@[section:end] Step completed: Cache Push"
